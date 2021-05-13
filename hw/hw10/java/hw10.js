c96ec13dd6ab6dcf6ba3970c0df156356262a662
amy10305 <80151273+amy10305@users.noreply.github.com>
2021-05-13 22:53:45 +0900

    Add files via upload

---
+++ hw/hw10/java/hw10.js
@@ -0,0 +1,28 @@
+id=prompt('아이디입력');
+    if(id=='std01') {
+        password=prompt('비밀번호 입력');
+        if (password==='1111'){
+            location.href="20_login.html"
+        }
+        else{
+            location.href="20_error.html"
+        }
+    }
+    else if(id=='std02') {
+        password=prompt('비밀번호 입력');
+        if (password==='2222'){
+            location.href="20_login.html"
+        }
+        else{
+            location.href="20_error.html"
+        }
+    }
+        else if(id=='std03') {
+        password=prompt('비밀번호 입력');
+        if (password==='3333'){
+            location.href="20_login.html"
+        }
+        else{
+            location.href="20_error.html"
+        }
+    }
